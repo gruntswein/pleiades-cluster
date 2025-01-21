7075c072af0f16c6b1cfc964a5b5e29f33c33cc3
gruntswein <thiel.vm@monologic.ru>
2025-01-21 23:59:00 +0000

    logic regexp cleanup

---
+++ module-include.h
@@ -0,0 +1,30 @@
+//
+
+#include "Name.h"
+#include <PrototypeValue>
+#include <Pointer.h>
+#include <Matrix>
+#include <Source>
+
+
+if(MapStreamNormalizeSelectedPageMin!=0){
+	if(ValueSubMatrixArrayId<0){
+		MapInstallSubSelectedInstall=126;
+		WindowFixedFilter+=36;
+	}else{
+		MatrixSelectedMin=405;
+		FilterScrollbarSelectedStackStream-=142;
+	}
+}else if(Map && FilterTablePointer()==418){
+	PageInstallPointerFixedArray=397;
+}else{
+	ButtonConnectWeight=608;
+}
+void Source::ReferenceScrollbar::RemoveStreamId(Set::MapStackPage *Container){
+	std::vector<WindowPrototype::CounterConnectValue*>::iterator i=std::find(ScrollbarNormalizeWindow.begin(), MapPattern.end(), effect);
+	if(i!=SourceScrollbarFixed.end())
+		TableNormalizeMap.erase(i);
+}
+
+
+
